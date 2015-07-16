c40ae45056d7c37610075805a9e99263c436a22d
griffio <griffio@users.noreply.github.com>
2015-07-16 16:50:29 +0100

    Resource/Link

---
+++ src/kollchap/link.clj
@@ -0,0 +1,8 @@
+(ns kollchap.link
+  (:require [schema.core :as s]
+            [ring.swagger.schema :refer [coerce!]]))
+
+(s/defschema Link {:href s/Str})
+
+(s/defschema Resource {:_links
+                       {:self Link}})
