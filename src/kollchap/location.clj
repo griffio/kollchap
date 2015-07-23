26b1f442d70646edb61ab4f827904abc681fde74
griffio <griffio@users.noreply.github.com>
2015-07-23 17:19:37 +0100

    Implement character location

--- src/kollchap/location.clj
+++ src/kollchap/location.clj
@@ -2,5 +2,7 @@
   (:require [kollchap.character :as c]
             [kollchap.room :as r]))
 
-(defn get-character-location [character-id] (r/get-room 5))
+(defn get-character-location [character-id]
+  (let [located-character (c/get-character character-id)]
+  (r/get-room (located-character :room-id))))
 
