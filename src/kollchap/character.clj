1db42d8b9ec1418f99bd48df16f9a65d61f70a1f
griffio <griffio@users.noreply.github.com>
2015-07-26 12:11:35 +0100

    Character room-key optional

--- src/kollchap/character.clj
+++ src/kollchap/character.clj
@@ -6,7 +6,7 @@
 (s/defschema GameCharacter {:id         Long
                             :name       String
                             :background String
-                            :room-key   String})
+                            (s/optional-key :room-key)  String})
 ;; Repository
 (defonce id-seq (atom 0))
 (defonce characters (atom (array-map)))
@@ -19,20 +19,17 @@
         character (coerce! GameCharacter (assoc new-character :id id))]
     (swap! characters assoc id character)
     character))
+
 ;; Data
 (when (empty? @characters)
   (add! {:name       "Slammer Kyntire"
-         :background "First-level Fighter searching for the Sword of the Sorcerer."
-         :room-key    ""})
+         :background "First-level Fighter searching for the Sword of the Sorcerer."})
   (add! {:name       "Hotfa Nap"
-         :background "First-level Sorceress from a nomad tribe in the Mesta Desert"
-         :room-key    ""})
+         :background "First-level Sorceress from a nomad tribe in the Mesta Desert."})
   (add! {:name       "Gripper 'The Skin' Longshank"
-         :background "First-level Thief from a tribe on the Albine empire border."
-         :room-key    ""})
+         :background "First-level Thief from a tribe on the Albine empire border."})
   (add! {:name       "Zhod Thobi"
-         :background "First-level Cleric joins party as N.P.C and receives equal share of treasure."
-         :room-key    ""})
+         :background "First-level Cleric joins party as N.P.C and receives equal share of treasure."})
   (add! {:name       "Belisarius"
          :background "First-level Thief N.P.C survivor. Currently hiding, if located will join party."
          :room-key    "5"})
