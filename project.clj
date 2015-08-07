d387099a1271d1860fe0648a712ad7ab9a0db35f
griffio <griffio@users.noreply.github.com>
2015-08-07 16:12:49 +0100

    ClojureScript deps

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [clj-time "0.9.0"] ;; needed as `lein ring` is broken.
                  [metosin/compojure-api "0.21.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/clojurescript "0.0-2371"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [lein-light-nrepl "0.1.0"]]
   :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
@@ -14,4 +15,12 @@
                        :aot :all}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                                   [midje "1.6.3"]]
-                   :plugins [[lein-ring "0.9.4"]]}})
+                   :plugins [[lein-ring "0.9.4"]
+                             [lein-cljsbuild "1.0.3"]]
+                   :cljsbuild {
+                      :builds [{
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                        :output-to "resources/public/main.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}]}}})
